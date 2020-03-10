12e069594a502836f6e201f1a45ef3a5a2644796
Wanderson Moura Oliveira <wmouraol@everis.com>
2020-03-10 17:59:13 -0300

    first commit

---
+++ src/app/flip-card/flip-card.component.ts
@@ -0,0 +1,14 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-flip-card',
+  templateUrl: './flip-card.component.html',
+  styleUrls: ['./flip-card.component.scss'],
+})
+export class FlipCardComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {}
+
+}
---
+++ src/app/home/home.page.ts
@@ -0,0 +1,14 @@
+import { Component, Input } from '@angular/core';
+import { from } from 'rxjs';
+
+@Component({
+  selector: 'app-home',
+  templateUrl: 'home.page.html',
+  styleUrls: ['home.page.scss'],
+})
+
+export class HomePage {
+
+  constructor() {}
+
+}
